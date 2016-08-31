78ef724b6e3e2a63035236baa2fe0f0a3892e469
Luka Stout <lukastout@gmail.com>
2016-08-31 14:42:26 +0200

    Version used in presentation

--- util.py
+++ util.py
@@ -4,39 +4,46 @@ import pandas as pd
 def load_offspring(df, fn):
     df_offspring = pd.read_json(fn)
     df_offspring.rename(columns={'child_id': 'Ind_ID'}, inplace=True)
-    df_offspring.set_index(['Exp_Num','Ind_ID'],inplace=True)
-
-
+    df_offspring.set_index(['Exp_Num', 'Ind_ID'], inplace=True)
     tallies = []
     for experiment_number in df.index.levels[0]:
         try:
             offspring_experiment = df_offspring.loc[experiment_number]
         except KeyError as e:
             continue
-        
+
         vc1 = offspring_experiment.parent1_id.value_counts()
         vc2 = offspring_experiment.parent2_id.value_counts()
         tally = vc1.add(vc2, fill_value=0)
         tally = tally.to_frame()
-        tally.columns=['numberOfChildren']
+        tally.columns = ['numberOfChildren']
         tally['Exp_Num'] = experiment_number
         tally.index = tally.index.rename('Ind_ID')
-        tally.set_index(['Exp_Num',tally.index], inplace=True)
+        tally.set_index(['Exp_Num', tally.index], inplace=True)
         tallies += [tally]
     df = df.join(pd.concat(tallies))
     return df
 
-def rank_by_birthtime(df):
-    temps = []
-    for experiment_number in df.index.levels[0]:
-        df_exp = df.loc[experiment_number]
-        temp_df = df_exp.birthtime.argsort()
-        temp_df = temp_df.to_frame()
-        temp_df.columns=['rankBirth']
-        temp_df['Exp_Num'] = experiment_number
-        temp_df.set_index(['Exp_Num',temp_df.index], inplace=True)
-        temp_df.loc[temp_df.rankBirth < 0, 'rankBirth'] = np.nan
-        temps.append(temp_df)
-    df = df.join(pd.concat(temps))
-    return df
+def load_meeting(df, fn):
+    df_meeting = pd.read_json(fn)
+    df_meeting.rename(columns={'indiv_id': 'Ind_ID'}, inplace=True)
+    df_meeting.set_index(['Exp_Num', 'Ind_ID'], inplace=True)
 
+    tallies = []
+    for experiment_number in df.index.levels[0]:
+        try:
+            meeting_experiment = df_meeting.loc[experiment_number]
+        except KeyError as e:
+            continue
+        meeting_experiment.reset_index(inplace=True)
+        vc1 = meeting_experiment.Ind_ID.value_counts()
+        vc2 = meeting_experiment.mate_indiv_id.value_counts()
+        tally = vc1.add(vc2, fill_value=0)
+        tally = tally.to_frame()
+        tally.columns = ['numberOfMeeting']
+        tally['Exp_Num'] = experiment_number
+        tally.index = tally.index.rename('Ind_ID')
+        tally.set_index(['Exp_Num', tally.index], inplace=True)
+        tallies += [tally]
+    df = df.join(pd.concat(tallies))
+    return df
